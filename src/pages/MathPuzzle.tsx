86739be0f59552bd888f3fee8e7e1a6d0247fc64
Adenhall <jamesadenhall@gmail.com>
2024-07-17 16:23:07 +0700

    feat: gameover mechanics

--- src/pages/MathPuzzle.tsx
+++ src/pages/MathPuzzle.tsx
@@ -18,7 +18,13 @@ const pingSfx = new Audio("ping.wav");
 const bloopSfx = new Audio("bloop.wav");
 
 const MathPuzzle = () => {
-  const { currentLevel } = useGameManager();
+  const {
+    currentLevel,
+    subtractScore,
+    maxNegativeScore,
+    negativeScore,
+    isGameOver,
+  } = useGameManager();
   const { rows, columns, addition, subtraction, multiplication, division } =
     MATH_PUZZLE_LEVELS[currentLevel];
 
@@ -28,22 +34,28 @@ const MathPuzzle = () => {
 
   const handleWrong = () => {
     bloopSfx.play();
+    subtractScore(1);
   };
 
   return (
     <>
       <Header />
-      <div className="w-full h-full p-6 h-5/6">
-        <JigsawPuzzle
-          key={`game-${currentLevel}`}
-          image="/vite.svg"
-          rows={rows}
-          columns={columns}
-          settings={{ addition, subtraction, multiplication, division }}
-          onCorrect={handleCorrect}
-          onWrong={handleWrong}
-        />
-      </div>
+      {isGameOver()
+        ? <h1>GAME OVER</h1>
+        : (
+          <div className="w-full h-full p-6 h-5/6">
+            <h1>Chances left: {maxNegativeScore - negativeScore}</h1>
+            <JigsawPuzzle
+              key={`game-${currentLevel}`}
+              image="/vite.svg"
+              rows={rows}
+              columns={columns}
+              settings={{ addition, subtraction, multiplication, division }}
+              onCorrect={handleCorrect}
+              onWrong={handleWrong}
+            />
+          </div>
+        )}
     </>
   );
 };
